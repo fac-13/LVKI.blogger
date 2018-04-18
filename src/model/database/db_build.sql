83081e903771dc3907aaf8c62f43688fe123e28e
missKatiaPunter <katiapunter@gmail.com>
2018-04-18 17:38:16 +0100

    adds drop table makes changes of data types

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -1,8 +1,10 @@
 BEGIN;
 
+DROP TABLE IF EXISTS users, posts CASCADE;
+
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL,
+    username VARCHAR(60) NOT NULL,
     hash_password VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     is_admin BOOLEAN DEFAULT FALSE
@@ -10,11 +12,13 @@ CREATE TABLE users (
 
 CREATE UNIQUE INDEX username_uniq_idx ON users (username);
 
+CREATE INDEX name_password_idx ON users (username, hash_password);
+
 CREATE TABLE posts (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) NOT NULL,
-    post_title VARCHAR(255) NOT NULL,
-    post_content TEXT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
     posted_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
