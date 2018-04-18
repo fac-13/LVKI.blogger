6cdeeebd7c5afa4c14704c121222a2d0cddf3cd6
vlbee <32312712+vlbee@users.noreply.github.com>
2018-04-18 14:52:04 +0100

    add error signup login controller relates #5 relates #8

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,13 +8,15 @@ const home = require('./home');
 const post = require('./post');
 // const signup = require('./signup');
 // const login = require('./login');
-// const error = require('./error');
+const error = require('./error');
 
 // routes
 router.get('/', home.get);
 router.get('/post/:post_ID/:post_title', post.get);
-// router.use(error.client);
-// router.use(error.client);
+// router.use('/signup', signup.post);
+// router.use('/login', login.post);
+router.use(error.client);
+router.use(error.client);
 
 
 module.exports = router;
