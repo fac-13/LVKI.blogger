8b94cc2f8e675b3908d339be73552b75aa9fd71d
i2xzy <yitzchokr@live.com>
2018-04-19 14:23:59 +0100

    add new hbs files

--- src/model/database/db_connection.js
+++ src/model/database/db_connection.js
@@ -4,7 +4,7 @@ const pgp = require('pg-promise')();
 const url = require('url');
 require('env2')('./.env');
 
-let DB_URL = process.env.DATABASE_URL;
+let DB_URL = process.env.LVKI_DB_URL;
 
 if (process.env.NODE_ENV === 'test') {
   DB_URL = process.env.TEST_DATABASE_URL;
@@ -15,7 +15,6 @@ if (!DB_URL) throw new Error('Enviroment variable DATABASE_URL must be set');
 const params = url.parse(DB_URL);
 const [username, password] = params.auth.split(':');
 
-
 let options = {
   host: 'localhost',
   port: params.port,
@@ -25,6 +24,7 @@ let options = {
   password,
   ssl: params.hostname !== 'localhost',
 };
+console.log(options.password);
 
 if (process.env.TRAVIS === 'true') {
   options = {
