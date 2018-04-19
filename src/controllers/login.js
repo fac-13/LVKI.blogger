15c1d4d155d61681fd87b1662d630226e718c7fd
vlbee <32312712+vlbee@users.noreply.github.com>
2018-04-19 15:58:46 +0100

    add user log in

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,7 +1,33 @@
+const userLogIn = require('../model/queries/userLogIn');
+const cookieSesson = require('cookie-session');
+const bcrypt = require('bcryptjs');
+
 exports.get = (req, res) => {
   res.render('login');
 };
 
 exports.post = (req, res) => {
-  res.redirect('/');
+  console.log('req', req.body);
+  const userDetails = req.body;
+
+
+  userLogIn(userDetails.username)
+    .then((queryRes) => {
+      console.log(queryRes);
+      const hash = queryRes[0].hash_password;
+      return bcrypt.compare(userDetails.password, hash);
+    })
+    .then((verified) => {
+      if (verified) {
+        // issue cookie
+        console.log('verified:', verified);
+        req.session.username = userDetails.username;
+        req.session.loggedIn = true;
+        res.redirect('/');
+      } else {
+        // send error message
+        res.send('Access DENIED');
+      }
+    })
+    .catch(error => console.log(error));
 };
