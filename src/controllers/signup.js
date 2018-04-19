6ed72b7f55461fb611d64feec193ffb715ff01e6
vlbee <32312712+vlbee@users.noreply.github.com>
2018-04-19 15:34:28 +0100

    add sign up query

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,7 +1,19 @@
+const bcrypt = require('bcryptjs');
+const userSignUp = require('./../model/queries/userSignUp');
+
 exports.get = (req, res) => {
   res.render('signup');
 };
 
 exports.post = (req, res) => {
-  res.redirect('/');
+  const userDetails = req.body;
+  bcrypt
+    .hash(userDetails.password, 10)
+    .then(hash => userSignUp(userDetails.username, hash))
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
